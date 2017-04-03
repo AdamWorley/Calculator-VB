7cd998a5b9457b0a68ccd540a0093ec6f58ea8a4
Adam Worley <worley9@hotmail.co.uk>
2017-04-03 15:14:31 +0100

    Add project files.

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,295 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.periodBtn = New System.Windows.Forms.Button()
+        Me.zeroBtn = New System.Windows.Forms.Button()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.equalsBtn = New System.Windows.Forms.Button()
+        Me.plusBtn = New System.Windows.Forms.Button()
+        Me.threeBtn = New System.Windows.Forms.Button()
+        Me.twoBtn = New System.Windows.Forms.Button()
+        Me.oneBtn = New System.Windows.Forms.Button()
+        Me.multbtn = New System.Windows.Forms.Button()
+        Me.nineBtn = New System.Windows.Forms.Button()
+        Me.eightBtn = New System.Windows.Forms.Button()
+        Me.sevenBtn = New System.Windows.Forms.Button()
+        Me.minusBtn = New System.Windows.Forms.Button()
+        Me.sixBtn = New System.Windows.Forms.Button()
+        Me.fiveBtn = New System.Windows.Forms.Button()
+        Me.fourBtn = New System.Windows.Forms.Button()
+        Me.txtDisplay = New System.Windows.Forms.Label()
+        Me.divideBtn = New System.Windows.Forms.Button()
+        Me.sqrBtn = New System.Windows.Forms.Button()
+        Me.sqrtBtn = New System.Windows.Forms.Button()
+        Me.negativeBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'periodBtn
+        '
+        Me.periodBtn.Location = New System.Drawing.Point(19, 153)
+        Me.periodBtn.Name = "periodBtn"
+        Me.periodBtn.Size = New System.Drawing.Size(29, 26)
+        Me.periodBtn.TabIndex = 0
+        Me.periodBtn.Text = "."
+        Me.periodBtn.UseVisualStyleBackColor = True
+        '
+        'zeroBtn
+        '
+        Me.zeroBtn.Location = New System.Drawing.Point(54, 153)
+        Me.zeroBtn.Name = "zeroBtn"
+        Me.zeroBtn.Size = New System.Drawing.Size(29, 26)
+        Me.zeroBtn.TabIndex = 1
+        Me.zeroBtn.Text = "0"
+        Me.zeroBtn.UseVisualStyleBackColor = True
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(89, 153)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(29, 26)
+        Me.clearBtn.TabIndex = 2
+        Me.clearBtn.Text = "CLR"
+        Me.clearBtn.UseVisualStyleBackColor = True
+        '
+        'equalsBtn
+        '
+        Me.equalsBtn.Location = New System.Drawing.Point(159, 153)
+        Me.equalsBtn.Name = "equalsBtn"
+        Me.equalsBtn.Size = New System.Drawing.Size(29, 26)
+        Me.equalsBtn.TabIndex = 3
+        Me.equalsBtn.Text = "="
+        Me.equalsBtn.UseVisualStyleBackColor = True
+        '
+        'plusBtn
+        '
+        Me.plusBtn.Location = New System.Drawing.Point(124, 57)
+        Me.plusBtn.Name = "plusBtn"
+        Me.plusBtn.Size = New System.Drawing.Size(29, 26)
+        Me.plusBtn.TabIndex = 7
+        Me.plusBtn.Text = "+"
+        Me.plusBtn.UseVisualStyleBackColor = True
+        '
+        'threeBtn
+        '
+        Me.threeBtn.Location = New System.Drawing.Point(89, 121)
+        Me.threeBtn.Name = "threeBtn"
+        Me.threeBtn.Size = New System.Drawing.Size(29, 26)
+        Me.threeBtn.TabIndex = 6
+        Me.threeBtn.Text = "3"
+        Me.threeBtn.UseVisualStyleBackColor = True
+        '
+        'twoBtn
+        '
+        Me.twoBtn.Location = New System.Drawing.Point(54, 121)
+        Me.twoBtn.Name = "twoBtn"
+        Me.twoBtn.Size = New System.Drawing.Size(29, 26)
+        Me.twoBtn.TabIndex = 5
+        Me.twoBtn.Text = "2"
+        Me.twoBtn.UseVisualStyleBackColor = True
+        '
+        'oneBtn
+        '
+        Me.oneBtn.Location = New System.Drawing.Point(19, 121)
+        Me.oneBtn.Name = "oneBtn"
+        Me.oneBtn.Size = New System.Drawing.Size(29, 26)
+        Me.oneBtn.TabIndex = 4
+        Me.oneBtn.Text = "1"
+        Me.oneBtn.UseVisualStyleBackColor = True
+        '
+        'multbtn
+        '
+        Me.multbtn.Location = New System.Drawing.Point(124, 121)
+        Me.multbtn.Name = "multbtn"
+        Me.multbtn.Size = New System.Drawing.Size(29, 26)
+        Me.multbtn.TabIndex = 15
+        Me.multbtn.Text = "*"
+        Me.multbtn.UseVisualStyleBackColor = True
+        '
+        'nineBtn
+        '
+        Me.nineBtn.Location = New System.Drawing.Point(89, 57)
+        Me.nineBtn.Name = "nineBtn"
+        Me.nineBtn.Size = New System.Drawing.Size(29, 26)
+        Me.nineBtn.TabIndex = 14
+        Me.nineBtn.Text = "9"
+        Me.nineBtn.UseVisualStyleBackColor = True
+        '
+        'eightBtn
+        '
+        Me.eightBtn.Location = New System.Drawing.Point(54, 57)
+        Me.eightBtn.Name = "eightBtn"
+        Me.eightBtn.Size = New System.Drawing.Size(29, 26)
+        Me.eightBtn.TabIndex = 13
+        Me.eightBtn.Text = "8"
+        Me.eightBtn.UseVisualStyleBackColor = True
+        '
+        'sevenBtn
+        '
+        Me.sevenBtn.Location = New System.Drawing.Point(19, 57)
+        Me.sevenBtn.Name = "sevenBtn"
+        Me.sevenBtn.Size = New System.Drawing.Size(29, 26)
+        Me.sevenBtn.TabIndex = 12
+        Me.sevenBtn.Text = "7"
+        Me.sevenBtn.UseVisualStyleBackColor = True
+        '
+        'minusBtn
+        '
+        Me.minusBtn.Location = New System.Drawing.Point(124, 89)
+        Me.minusBtn.Name = "minusBtn"
+        Me.minusBtn.Size = New System.Drawing.Size(29, 26)
+        Me.minusBtn.TabIndex = 11
+        Me.minusBtn.Text = "-"
+        Me.minusBtn.UseVisualStyleBackColor = True
+        '
+        'sixBtn
+        '
+        Me.sixBtn.Location = New System.Drawing.Point(89, 89)
+        Me.sixBtn.Name = "sixBtn"
+        Me.sixBtn.Size = New System.Drawing.Size(29, 26)
+        Me.sixBtn.TabIndex = 10
+        Me.sixBtn.Text = "6"
+        Me.sixBtn.UseVisualStyleBackColor = True
+        '
+        'fiveBtn
+        '
+        Me.fiveBtn.Location = New System.Drawing.Point(54, 89)
+        Me.fiveBtn.Name = "fiveBtn"
+        Me.fiveBtn.Size = New System.Drawing.Size(29, 26)
+        Me.fiveBtn.TabIndex = 9
+        Me.fiveBtn.Text = "5"
+        Me.fiveBtn.UseVisualStyleBackColor = True
+        '
+        'fourBtn
+        '
+        Me.fourBtn.Location = New System.Drawing.Point(19, 89)
+        Me.fourBtn.Name = "fourBtn"
+        Me.fourBtn.Size = New System.Drawing.Size(29, 26)
+        Me.fourBtn.TabIndex = 8
+        Me.fourBtn.Text = "4"
+        Me.fourBtn.UseVisualStyleBackColor = True
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.AutoSize = True
+        Me.txtDisplay.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.txtDisplay.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.txtDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDisplay.Location = New System.Drawing.Point(10, 19)
+        Me.txtDisplay.MinimumSize = New System.Drawing.Size(200, 0)
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.Size = New System.Drawing.Size(200, 20)
+        Me.txtDisplay.TabIndex = 16
+        Me.txtDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'divideBtn
+        '
+        Me.divideBtn.Location = New System.Drawing.Point(124, 153)
+        Me.divideBtn.Name = "divideBtn"
+        Me.divideBtn.Size = New System.Drawing.Size(29, 26)
+        Me.divideBtn.TabIndex = 17
+        Me.divideBtn.Text = "/"
+        Me.divideBtn.UseVisualStyleBackColor = True
+        '
+        'sqrBtn
+        '
+        Me.sqrBtn.Location = New System.Drawing.Point(159, 57)
+        Me.sqrBtn.Name = "sqrBtn"
+        Me.sqrBtn.Size = New System.Drawing.Size(29, 26)
+        Me.sqrBtn.TabIndex = 18
+        Me.sqrBtn.Text = "sqr"
+        Me.sqrBtn.UseVisualStyleBackColor = True
+        '
+        'sqrtBtn
+        '
+        Me.sqrtBtn.Location = New System.Drawing.Point(159, 89)
+        Me.sqrtBtn.Name = "sqrtBtn"
+        Me.sqrtBtn.Size = New System.Drawing.Size(29, 26)
+        Me.sqrtBtn.TabIndex = 19
+        Me.sqrtBtn.Text = "√"
+        Me.sqrtBtn.UseVisualStyleBackColor = True
+        '
+        'negativeBtn
+        '
+        Me.negativeBtn.Location = New System.Drawing.Point(159, 121)
+        Me.negativeBtn.Name = "negativeBtn"
+        Me.negativeBtn.Size = New System.Drawing.Size(29, 26)
+        Me.negativeBtn.TabIndex = 20
+        Me.negativeBtn.Text = "inv"
+        Me.negativeBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(222, 195)
+        Me.Controls.Add(Me.negativeBtn)
+        Me.Controls.Add(Me.sqrtBtn)
+        Me.Controls.Add(Me.sqrBtn)
+        Me.Controls.Add(Me.divideBtn)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.Controls.Add(Me.multbtn)
+        Me.Controls.Add(Me.nineBtn)
+        Me.Controls.Add(Me.eightBtn)
+        Me.Controls.Add(Me.sevenBtn)
+        Me.Controls.Add(Me.minusBtn)
+        Me.Controls.Add(Me.sixBtn)
+        Me.Controls.Add(Me.fiveBtn)
+        Me.Controls.Add(Me.fourBtn)
+        Me.Controls.Add(Me.plusBtn)
+        Me.Controls.Add(Me.threeBtn)
+        Me.Controls.Add(Me.twoBtn)
+        Me.Controls.Add(Me.oneBtn)
+        Me.Controls.Add(Me.equalsBtn)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Controls.Add(Me.zeroBtn)
+        Me.Controls.Add(Me.periodBtn)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents periodBtn As Button
+    Friend WithEvents zeroBtn As Button
+    Friend WithEvents clearBtn As Button
+    Friend WithEvents equalsBtn As Button
+    Friend WithEvents plusBtn As Button
+    Friend WithEvents threeBtn As Button
+    Friend WithEvents twoBtn As Button
+    Friend WithEvents oneBtn As Button
+    Friend WithEvents multbtn As Button
+    Friend WithEvents nineBtn As Button
+    Friend WithEvents eightBtn As Button
+    Friend WithEvents sevenBtn As Button
+    Friend WithEvents minusBtn As Button
+    Friend WithEvents sixBtn As Button
+    Friend WithEvents fiveBtn As Button
+    Friend WithEvents fourBtn As Button
+    Friend WithEvents txtDisplay As Label
+    Friend WithEvents divideBtn As Button
+    Friend WithEvents sqrBtn As Button
+    Friend WithEvents sqrtBtn As Button
+    Friend WithEvents negativeBtn As Button
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Dim num1 As Double
+    Dim num2 As Double
+    Dim operation
+
+    ' Operator handling
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+        txtDisplay.Text = ""
+    End Sub
+
+    Private Sub plusBtn_Click(sender As Object, e As EventArgs) Handles plusBtn.Click
+        operation = "+"
+        Double.TryParse(txtDisplay.Text, num1)
+        txtDisplay.Text = ""
+    End Sub
+
+    Private Sub minusBtn_Click(sender As Object, e As EventArgs) Handles minusBtn.Click
+        operation = "-"
+        Double.TryParse(txtDisplay.Text, num1)
+        txtDisplay.Text = ""
+    End Sub
+
+    Private Sub multbtn_Click(sender As Object, e As EventArgs) Handles multbtn.Click
+        operation = "*"
+        Double.TryParse(txtDisplay.Text, num1)
+        txtDisplay.Text = ""
+    End Sub
+
+    Private Sub divideBtn_Click(sender As Object, e As EventArgs) Handles divideBtn.Click
+        operation = "/"
+        Double.TryParse(txtDisplay.Text, num1)
+        txtDisplay.Text = ""
+    End Sub
+
+    Private Sub periodBtn_Click(sender As Object, e As EventArgs) Handles periodBtn.Click
+        If Not Regex.IsMatch(txtDisplay.Text, "[\.]") Then
+            txtDisplay.Text = txtDisplay.Text & "."
+        End If
+    End Sub
+
+    Private Sub negativeBtn_Click(sender As Object, e As EventArgs) Handles negativeBtn.Click
+        Dim number
+        Double.TryParse(txtDisplay.Text, number)
+        txtDisplay.Text = number * -1
+    End Sub
+
+    Private Sub equalsBtn_Click(sender As Object, e As EventArgs) Handles equalsBtn.Click
+        If txtDisplay.Text IsNot Nothing Then
+            Double.TryParse(txtDisplay.Text, num2)
+            If operation Is "+" Then
+                txtDisplay.Text = num1 + num2
+            ElseIf operation Is "-" Then
+                txtDisplay.Text = num1 - num2
+            ElseIf operation Is "*" Then
+                txtDisplay.Text = num1 * num2
+            ElseIf operation Is "/" Then
+                If num2 = 0 Then
+                    txtDisplay.Text = "Error"
+                Else
+                    txtDisplay.Text = num1 / num2
+                End If
+
+            End If
+            operation = ""
+        End If
+
+    End Sub
+
+
+    ' Number button handling
+    Private Sub zeroBtn_Click(sender As Object, e As EventArgs) Handles zeroBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 0
+    End Sub
+
+
+    Private Sub oneBtn_Click(sender As Object, e As EventArgs) Handles oneBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 1
+    End Sub
+
+    Private Sub twoBtn_Click(sender As Object, e As EventArgs) Handles twoBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 2
+    End Sub
+
+    Private Sub threeBtn_Click(sender As Object, e As EventArgs) Handles threeBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 3
+    End Sub
+
+    Private Sub fourBtn_Click(sender As Object, e As EventArgs) Handles fourBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 4
+    End Sub
+
+    Private Sub fiveBtn_Click(sender As Object, e As EventArgs) Handles fiveBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 5
+    End Sub
+
+    Private Sub sixBtn_Click(sender As Object, e As EventArgs) Handles sixBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 6
+    End Sub
+
+    Private Sub sevenBtn_Click(sender As Object, e As EventArgs) Handles sevenBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 7
+    End Sub
+
+    Private Sub eightBtn_Click(sender As Object, e As EventArgs) Handles eightBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 8
+    End Sub
+
+    Private Sub nineBtn_Click(sender As Object, e As EventArgs) Handles nineBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        txtDisplay.Text = txtDisplay.Text & 9
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles sqrBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        Dim sqr
+        Double.TryParse(txtDisplay.Text, sqr)
+        txtDisplay.Text = sqr * sqr
+    End Sub
+
+    Private Sub sqrtBtn_Click(sender As Object, e As EventArgs) Handles sqrtBtn.Click
+        If txtDisplay.Text = "Error" Then
+            txtDisplay.Text = ""
+        End If
+        Dim sqrrt
+        Double.TryParse(txtDisplay.Text, sqrrt)
+        txtDisplay.Text = sqrrt ^ 0.5
+    End Sub
+
+
+End Class
